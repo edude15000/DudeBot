0c03d1a2559fa947e1a068f0b002759d88e3f28f
EVAN\Evan Snitkey <edude15000@hotmail.com>
2018-02-11 01:49:52 -0600

    3.4.0

--- TwitchBot/WpfApplication1/Utils.cs
+++ TwitchBot/WpfApplication1/Utils.cs
@@ -11,8 +11,8 @@ using Newtonsoft.Json.Linq;
 
 public class Utils
 {
-    public static String version = "3.3.0"; // UPDATE AS NECESSARY
-    public static String releaseDate = "2/8/2018"; // UPDATE AS NECESSARY
+    public static String version = "3.4.0"; // UPDATE AS NECESSARY
+    public static String releaseDate = "2/11/2018"; // UPDATE AS NECESSARY
     public static String twitchClientID = "c203ik5st5i3kde6amsageei2snaj1v";
     public static String botMaker = "edude15000";
     public static String currentSongFile = @"bin\currentsong.txt";
